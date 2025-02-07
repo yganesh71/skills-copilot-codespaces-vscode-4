9b9c617dd163329dcf2529b778594a66e4494651
yganesh71 <144485613+yganesh71@users.noreply.github.com>
2025-02-07 11:43:24 +0000

    Copilot First Commit

---
+++ skills.js
@@ -0,0 +1,8 @@
+function calculateNumbers(var1, var2) {
+  var sum = var1 + var2;
+  var difference = var1 - var2;
+  var product = var1 * var2;
+  var quotient = var1 / var2;
+
+  return [sum, difference, product, quotient];
+}
